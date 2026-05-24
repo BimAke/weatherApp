dbe82ad474a6e6cb651bb8af83e45e8abd4ef887
BimAke <144439636+BimAke@users.noreply.github.com>
2026-05-24 23:59:47 +0600

    Add WeatherApi interface for fetching weather data

---
+++ app/src/main/java/com/example/data/remote/WeatherApi.kt
@@ -0,0 +1,17 @@
+package com.example.data.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+
+    @GET("weather")
+    suspend fun getWeather(
+
+        @Query("q")
+        city: String,
+
+        @Query("appid")
+        apiKey: String
+    )
+}
