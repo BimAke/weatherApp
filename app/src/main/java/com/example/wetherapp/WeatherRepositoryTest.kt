e228b0f03e44f6395b836cfc849d7ca4cc7b106d
BimAke <144439636+BimAke@users.noreply.github.com>
2026-05-25 00:29:12 +0600

    Create WeatherRepositoryTest.kt

---
+++ app/src/main/java/com/example/wetherapp/WeatherRepositoryTest.kt
@@ -0,0 +1,13 @@
+package com.example.weatherapp
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class WeatherRepositoryTest {
+
+    @Test
+    fun testRepository() {
+
+        assertTrue(true)
+    }
+}
