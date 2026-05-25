ea0922d567daad4683a887d52fff918f5231b41e
BimAke <144439636+BimAke@users.noreply.github.com>
2026-05-25 17:12:30 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/wetherapp/MainActivity.kt
+++ app/src/main/java/com/example/wetherapp/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.example.weatherapp
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,30 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-
-        val searchInput = findViewById<EditText>(
-            R.id.searchInput
-        )
-
-        val searchButton = findViewById<Button>(
-            R.id.searchButton
-        )
-
-        val cityText = findViewById<TextView>(
-            R.id.cityText
-        )
-
-        val tempText = findViewById<TextView>(
-            R.id.tempText
-        )
-
-        searchButton.setOnClickListener {
-
-            val city = searchInput.text.toString()
-
-            cityText.text = city
-
-            tempText.text = "22°C"
-        }
     }
 }
